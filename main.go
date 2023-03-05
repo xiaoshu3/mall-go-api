a3e4c23945fd170272918dfe50b13e7204d3dbfc
xiaoshu <longbin314@gmail.com>
2023-03-05 22:23:38 +0800

    配置文件

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"mall/bootstrap"
+	"mall/pkg/config"
+
+	"github.com/gin-gonic/gin"
+
+	btsConfig "mall/config"
+)
+
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
 
 func main() {
-	fmt.Printf("hello")
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+	// new 一个 Gin Engine 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	err := router.Run(":8010")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
