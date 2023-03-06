6123f7c90d89f64af13a820a49fef630a154e80f
xiaoshu <longbin314@gmail.com>
2023-03-06 17:22:12 +0800

    JSON 解析和验证器封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -1,9 +1,9 @@
 package auth
 
 import (
-	"fmt"
 	v1 "mall/app/http/controllers/api/v1"
 	"mall/app/models/user"
+	"mall/app/requests"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,22 +16,35 @@ type SignupController struct {
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	// 	请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
+	// type PhoneExistRequest struct {
+	// 	Phone string `json:"phone"`
+	// }
 
-	request := PhoneExistRequest{}
+	request := requests.SignupPhoneExistRequest{}
 
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		fmt.Println(err.Error())
+	// if err := c.ShouldBindJSON(&request); err != nil {
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// //表单验证
+
+	// errs := requests.ValidateSignupPhoneExist(&request, c)
+	// // errs 返回长度等于零即通过，大于 0 即有错误发生
+	// if len(errs) > 0 {
+	// 	// 验证失败，返回 422 状态码和错误信息
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"errors": errs,
+	// 	})
+	// 	return
+	// }
+
+	if ok := requests.Validate(c, &request, requests.ValidateSignupPhoneExist); !ok {
 		return
 	}
-
-	fmt.Println("-----------------------------")
-	fmt.Println(request.Phone)
 
 	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
