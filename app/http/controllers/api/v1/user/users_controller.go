a19c0a3139e0ccfa0af089b3ec8e1c7a45f6b99c
xiaoshu <longbin314@gmail.com>
2023-04-10 14:55:22 +0800

    用户列表

--- app/http/controllers/api/v1/user/users_controller.go
+++ app/http/controllers/api/v1/user/users_controller.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	v1 "mall/app/http/controllers/api/v1"
+	"mall/app/models/user"
 	"mall/pkg/auth"
 	"mall/pkg/response"
 
@@ -16,4 +17,10 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 	userModel := auth.CurrentUser(c)
 	response.Data(c, userModel)
+}
+
+// Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }
